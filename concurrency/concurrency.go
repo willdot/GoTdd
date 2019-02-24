cbd7d8ce1d7795ab33369ba1695777d6d5c64f48
Will <will7989@hotmail.com>
2019-02-24 11:36:42 +0000

    finished concurrency

--- concurrency/concurrency.go
+++ concurrency/concurrency.go
@@ -3,12 +3,25 @@ package concurrency
 // WebsiteChecker checks the status of a url input
 type WebsiteChecker func(string) bool
 
+type result struct {
+	string
+	bool
+}
+
 // CheckWebsites takes an array of urls and checks the status
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultChannel := make(chan result)
 
 	for _, url := range urls {
-		results[url] = wc(url)
+		go func(u string) {
+			resultChannel <- result{u, wc(u)}
+		}(url)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		result := <-resultChannel
+		results[result.string] = result.bool
 	}
 
 	return results
